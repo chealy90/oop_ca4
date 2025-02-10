b6acf54b22ad61835bc2472e9adeacb8f2ffac4f
chealy90 <christopherhealy20@gmail.com>
2025-02-10 11:39:46 +0000

    Project structure setup

---
+++ src/main/java/BusinessObjects/App.java
@@ -0,0 +1,13 @@
+package BusinessObjects;
+
+public class App {
+    public static void main(String[] args) {
+        App app = new App();
+        app.start();
+    }
+
+
+    public void start(){
+
+    }
+}
